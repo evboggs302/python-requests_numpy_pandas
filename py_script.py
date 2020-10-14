f12256d0ea31942cfec3f6c6e05112a912843e54
evboggs302 <evboggs94@gmail.com>
2020-10-14 10:53:45 -0400

    dataframe to csv created

--- py_script.py
+++ py_script.py
@@ -4,15 +4,16 @@ import numpy as np
 import requests
 import re
 
-# Testing requests with Star Wars api.
-sw_people = requests.get('https://swapi.dev/api/people/?page=9').text
-# sw_films = requests.get('https://swapi.dev/api/films').text
-# sw_planets = requests.get('https://swapi.dev/api/planets').text
-# sw_starships = requests.get('https://swapi.dev/api/starships').text
-# sw_vehicles = requests.get('https://swapi.dev/api/vehicles').text
+# TESTING STARWARS API
+sw_films = requests.get('https://swapi.dev/api/films').text
+sw_planets = requests.get('https://swapi.dev/api/planets').text
+sw_starships = requests.get('https://swapi.dev/api/starships').text
+sw_vehicles = requests.get('https://swapi.dev/api/vehicles').text
 # USE CONTEXT MANAGERS TO WRITE TO FILES - they automatically close dependencies after completed action
-with open('starwars_people.json', 'a') as f:
-    f.write(sw_people.encode('utf-8'))
+# for x in range(1,10):
+#     sw_people = requests.get('https://swapi.dev/api/people/?page={}'.format(x)).text
+#     with open('starwars_people.json', 'a') as f:
+#         f.write(sw_people.encode('utf-8'))
 # with open('starwars_films.json', 'w') as f:
 #     f.write(sw_films.encode('utf-8'))
 # with open('starwars_planets.json', 'w') as f:
@@ -24,7 +25,7 @@ with open('starwars_people.json', 'a') as f:
 # print('Files written successfully.')
 
 
-# Finance API to eventually use NumPy and Pandas.
+# FINANCE API SETUP FOR NUMPY AND PANDAS
 income_url = "https://financial-modeling-prep.p.rapidapi.com/income-statement/AAPL"
 cashflow_url = "https://financial-modeling-prep.p.rapidapi.com/cash-flow-statement/AAPL"
 balance_url = "https://financial-modeling-prep.p.rapidapi.com/balance-sheet-statement/AAPL"
@@ -53,3 +54,11 @@ profile = requests.request(
 # with open('profiles.json', 'w') as f:
 #     f.write(profile.text)
 # print('Files written successfully.')
+
+# NUMPY AND PANDS MANIPULATION
+peeps = pd.read_json("./starwars_people.json")
+peep_data = []
+for x in peeps.res:
+    peep_data += x['results']
+peep_df = pd.DataFrame(peep_data)
+peep_df.to_csv(path_or_buf='./sw_peeps.csv', index=False, encoding='utf-8')
